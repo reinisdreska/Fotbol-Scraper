1335c84c5b4d44878eecdcec4e451fe8d061029c
reinis dreska <rdreska@gmail.com>
2021-11-02 17:31:10 +0200

    atrod klubs 1

--- scraper.py
+++ scraper.py
@@ -1,7 +1,6 @@
 import requests
-import time
 from bs4 import BeautifulSoup as bs
-import csv
+import datetime
 
 URL = "https://lff.lv/sacensibas/viriesi/optibet-virsliga/"
 LAPAS = "lapas/"
@@ -39,27 +38,50 @@ def info(datne):
             continue
         spele["menesis"] = tags
         spele["menesis"] = str(spele["menesis"])
-        size = len(spele["menesis"])
-        spele["menesis"] = spele["menesis"][5:size - 6]
-        #print(spele["menesis"])
+        spele["menesis"] = spele["menesis"][5:- 6]
+        if spele["menesis"] == "kārta":
+            continue
+        menesi = ["jan", "feb", "mar", "apr", "mai", "jun", "jūl", "aug", "sep","nov", "dec"]
+        for menesis in range(1, len(menesi)+1):
+            spele["menesis"] = menesis
+            print(spele["menesis"])
 
         tags = row.find_all("h5")
         if not tags:
             continue
         spele["diena"] = tags
         spele["diena"] = str(spele["diena"])
-        size = len(spele["diena"])
-        spele["diena"] = spele["diena"][5:size - 6]
+        spele["diena"] = spele["diena"][5: - 6]
         #print(spele["diena"])
+
+        tags = row.find_all("div", class_="time")
+        if not tags:
+            continue
+        spele["laiks"] = tags
+        spele["laiks"] = str(spele["laiks"])
+        spele["laiks"] = spele["laiks"][19:- 7]
+        #print(spele["laiks"])
+
+        # print('Timestamp: {:%m-%d}'.format(datetime.datetime(spele["menesis"], spele["diena"])))
+        # spele["timestamp"] = datetime.date(spele["menesis"], spele["diena"], spele["laiks"])
+        #print(spele["timestamp"])
 
         tags = row.find_all("div", class_="stadium")
         if not tags:
             continue
         spele["arena"] = tags
         spele["arena"] = str(spele["arena"])
-        size = len(spele["arena"])
-        spele["arena"] = spele["arena"][22:size - 7]
+        spele["arena"] = spele["arena"][22:- 7]
         #print(spele["arena"])
+
+        tags = row.find_all("a")
+        if not tags:
+            continue
+        spele["klubs1"] = tags[0]
+        spele["klubs1"] = str(spele["klubs1"])
+        sub_str = ">"
+        spele["klubs1"] = spele["klubs1"][spele["klubs1"].index(sub_str) + len(sub_str):- 4]
+        #print(spele["klubs1"])
         
 
 info(f"lapas/1_lapa.html")
