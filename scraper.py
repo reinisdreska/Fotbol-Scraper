d1e859501979c81d870713e993ae02d0668a4fa7
reinis dreska <rdreska@gmail.com>
2021-11-02 15:34:56 +0200

    dabun stadiona nosaukumu

--- scraper.py
+++ scraper.py
@@ -19,4 +19,35 @@ def saglabat(url, datne):
 def lejupieladet_lapas():
     saglabat(f"{URL}", f"1_lapa.html")
 
-lejupieladet_lapas()
+#lejupieladet_lapas()
+
+def info(datne):
+    dati = []
+    with open(datne, 'r', encoding='UTF-8') as f:
+        html = f.read()
+
+    base = bs(html, "html.parser")
+
+    galvena = base.find_all("tr")
+    
+
+    # print(galvena)
+    # for i in range(1, len(galvena)):
+    #     print("==========================")
+    #     print(i, galvena[i].get_text())
+    # print("==========================")
+
+    
+
+    for row in galvena:
+        spele = {}
+        tags = row.find_all("div", class_="stadium")
+        if not tags:
+            continue
+        spele["arena"] = tags
+        spele["arena"] =str(spele["arena"])
+        size = len(spele["arena"])
+        spele["arena"] = spele["arena"][22:size - 7]
+        print(spele["arena"])
+
+info(f"lapas/1_lapa.html")
